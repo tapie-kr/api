c9d7f9e418f5342186d4094bbd2e62debd47bbfa
Yuchan Han <serltretu24@gmail.com>
2025-01-30 20:11:26 +0900

    ✨ Enhance Google OAuth strategy with dynamic service-based callback URL

--- packages/api/src/auth/strategies/google.strategy.ts
+++ packages/api/src/auth/strategies/google.strategy.ts
@@ -3,6 +3,7 @@ import { Profile, Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { GoogleAuthDto } from '../dto/google-auth.dto';
+import { Request } from 'express';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
@@ -12,10 +13,23 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
       callbackURL: configService.get('GOOGLE_CALLBACK_URL'),
       scope: ['email', 'profile'],
+      passReqToCallback: true,
+    });
+  }
+
+  authenticate(req: Request, options?: any) {
+    const serviceName = req.query.service as string;
+    const callbackURLContext = new URL(this.configService.get('GOOGLE_CALLBACK_URL'));
+    callbackURLContext.searchParams.append('service', serviceName || 'website');
+    const callbackURL = callbackURLContext.toString();
+    super.authenticate(req, {
+      ...options,
+      callbackURL,
     });
   }
 
   async validate(
+    _req: Request,
     accessToken: string,
     _refreshToken: string,
     profile: Profile,
