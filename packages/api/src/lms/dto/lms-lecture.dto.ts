876f669d69e3575474c2a1f3123510245422f032
iamfiro <appturbo102@gmail.com>
2025-01-31 19:01:06 +0900

    🔧 Update LectureDto: "강의" -> "수업"

--- packages/api/src/lms/dto/lms-lecture.dto.ts
+++ packages/api/src/lms/dto/lms-lecture.dto.ts
@@ -1,42 +1,41 @@
-import {
-  IsDateString,
-  IsEnum,
-  IsNumber,
-  IsString,
-  ValidateNested,
-} from 'class-validator';
+import { IsDateString, IsEnum, IsNumber, IsString, ValidateNested } from 'class-validator';
 
 import { Type } from 'class-transformer';
 import { UnitType } from 'src/common/enum/unit-type';
-import { ApiProperty } from '@nestjs/swagger'
-import { HostDto } from './lms-host.dto'
+import { ApiProperty } from '@nestjs/swagger';
+import { HostDto } from './lms-host.dto';
 
 export class LectureDto {
   @ApiProperty({
-    description: '강의 ID',
+    description: '수업 ID',
     example: 'lectureId123',
   })
   @IsString()
   id: string;
 
   @ApiProperty({
-    description: '강의 호스트 정보',
-    example: { id: 'host123', nickname: '김태영영', username: 'tyeongkim', avatar: 'c35fe23b48593b263af460ae1aa02cdd' }
+    description: '수업 호스트 정보',
+    example: {
+      id: 'host123',
+      nickname: '김태영영',
+      username: 'tyeongkim',
+      avatar: 'c35fe23b48593b263af460ae1aa02cdd',
+    },
   })
   @ValidateNested()
   @Type(() => HostDto)
   host: HostDto;
 
   @ApiProperty({
-    description: '강의를 진행하는 유닛',
+    description: '수업를 진행하는 유닛',
     example: UnitType.DESIGNER,
   })
   @IsEnum(UnitType)
   unit: UnitType;
 
   @ApiProperty({
-    description: '강의 시작 시간',
-    example: 1738297059
+    description: '수업 시작 시간',
+    example: 1738297059,
   })
   @IsNumber()
   startTime: number;
