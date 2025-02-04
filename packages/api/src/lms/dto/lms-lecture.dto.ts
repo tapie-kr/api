c253fc5c45e0fb988327abc7764b93794b1d34ab
Yuchan Han <serltretu24@gmail.com>
2025-02-04 09:36:14 +0900

    🔧 Remove Swagger decorators and dependencies from API modules

--- packages/api/src/lms/dto/lms-lecture.dto.ts
+++ packages/api/src/lms/dto/lms-lecture.dto.ts
@@ -2,41 +2,19 @@ import { IsDateString, IsEnum, IsNumber, IsString, ValidateNested } from 'class-
 
 import { Type } from 'class-transformer';
 import { UnitType } from 'src/common/enum/unit-type';
-import { ApiProperty } from '@nestjs/swagger';
 import { HostDto } from './lms-host.dto';
 
 export class LectureDto {
-  @ApiProperty({
-    description: '수업 ID',
-    example: 'lectureId123',
-  })
   @IsString()
   id: string;
 
-  @ApiProperty({
-    description: '수업 호스트 정보',
-    example: {
-      id: 'host123',
-      nickname: '김태영영',
-      username: 'tyeongkim',
-      avatar: 'c35fe23b48593b263af460ae1aa02cdd',
-    },
-  })
   @ValidateNested()
   @Type(() => HostDto)
   host: HostDto;
 
-  @ApiProperty({
-    description: '수업를 진행하는 유닛',
-    example: UnitType.DESIGNER,
-  })
   @IsEnum(UnitType)
   unit: UnitType;
 
-  @ApiProperty({
-    description: '수업 시작 시간',
-    example: 1738297059,
-  })
   @IsNumber()
   startTime: number;
 }
--- packages/api/src/members/dto/profile-link.dto.ts
+++ packages/api/src/members/dto/profile-link.dto.ts
@@ -1,28 +1,22 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsUUID, IsNumber } from 'class-validator';
 
 export class ProfileLinkDto {
-  @ApiProperty({ description: '링크 고유 UUID' })
   @IsNumber()
   @IsNotEmpty()
   id: number;
 
-  @ApiProperty({ description: '프로필 UUID' })
   @IsUUID()
   @IsNotEmpty()
   memberUUID: string;
 
-  @ApiProperty({ description: '링크 아이콘' })
   @IsString()
   @IsNotEmpty()
   icon: string;
 
-  @ApiProperty({ description: '링크 이름' })
   @IsString()
   @IsNotEmpty()
   label: string;
 
-  @ApiProperty({ description: '링크 URL' })
   @IsString()
   @IsNotEmpty()
   href: string;
