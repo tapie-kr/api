7a0aff87daa1a4eb3fcdebcfdd8d25dcc2fc6fab
Yuchan Han <serltretu24@gmail.com>
2025-02-05 09:00:53 +0900

    ✨ Enhance ApplyForm service and repository with create, update, and delete functionalities

---
+++ packages/api/src/form/dto/form.dto.ts
@@ -0,0 +1,18 @@
+import { IsString, IsDate, IsNumber } from 'class-validator';
+
+export class ApplyFormDto {
+  @IsNumber()
+  id: number;
+
+  @IsString()
+  name: string;
+
+  @IsDate()
+  startsAt: Date;
+
+  @IsDate()
+  endsAt: Date;
+}
+
+export type CreateApplyFormDto = Omit<ApplyFormDto, 'id'>;
+export type UpdateApplyFormDto = Partial<CreateApplyFormDto>;
---
+++ packages/api/src/form/dto/response.dto.ts
@@ -0,0 +1,31 @@
+import { IsString, IsEnum, IsPhoneNumber, IsUUID } from 'class-validator';
+import { MemberUnit } from '@tapie-kr/api-database/client';
+
+export class FormResponseDto {
+  @IsUUID()
+  uuid: string;
+
+  @IsString()
+  name: string;
+
+  @IsEnum(MemberUnit)
+  unit: MemberUnit;
+
+  @IsPhoneNumber()
+  phoneNumber: string;
+
+  @IsString()
+  introduction: string;
+
+  @IsString()
+  motivation: string;
+
+  @IsString()
+  expectedActivities: string;
+
+  @IsString()
+  reasonToChoose: string;
+}
+
+export type CreateFormResponseDto = Omit<FormResponseDto, 'id'>;
+export type UpdateFormResponseDtoType = Partial<CreateFormResponseDto>;
