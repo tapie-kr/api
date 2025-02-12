4cc872e0ba8138e31446d1291ffed2cded4e448e
Yuchan Han <serltretu24@gmail.com>
2025-02-12 15:36:59 +0900

    ✨ Enhance DTOs with Swagger decorators for improved API documentation and validation

--- packages/api/src/form/dto/form.dto.ts
+++ packages/api/src/form/dto/form.dto.ts
@@ -1,18 +1,28 @@
-import { IsDate, IsNumber, IsString } from 'class-validator';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
+import { IsDateString, IsNumber, IsString } from 'class-validator';
 
 export class ApplyFormDto {
   @IsNumber()
   id: number;
 
   @IsString()
+  @ApiProperty({ description: '폼 이름' })
   name: string;
 
-  @IsDate()
+  @IsDateString()
+  @ApiProperty({ description: '시작 날짜' })
   startsAt: Date;
 
-  @IsDate()
+  @IsDateString()
+  @ApiProperty({ description: '종료 날짜' })
   endsAt: Date;
 }
 
-export type CreateApplyFormDto = Omit<ApplyFormDto, 'id'>;
-export type UpdateApplyFormDto = Partial<CreateApplyFormDto>;
+export type CreateApplyFormType = Omit<ApplyFormDto, 'id'>;
+export type UpdateApplyFormType = Partial<CreateApplyFormDto>;
+
+export class CreateApplyFormDto extends OmitType(ApplyFormDto, ['id'] as const) {
+}
+
+export class UpdateApplyFormDto extends PartialType(CreateApplyFormDto) {
+}
--- packages/api/src/form/dto/response.dto.ts
+++ packages/api/src/form/dto/response.dto.ts
@@ -1,4 +1,5 @@
-import { MemberUnit } from '@tapie-kr/api-database/client';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
+import { MemberUnit } from '@tapie-kr/api-database';
 import {
   IsEnum,
   IsPhoneNumber,
@@ -11,26 +12,41 @@ export class FormResponseDto {
   uuid: string;
 
   @IsString()
+  @ApiProperty({ description: '이름' })
   name: string;
 
   @IsEnum(MemberUnit)
+  @ApiProperty({
+    description: '유닛', enum: MemberUnit,
+  })
   unit: MemberUnit;
 
   @IsPhoneNumber()
+  @ApiProperty({ description: '전화번호' })
   phoneNumber: string;
 
   @IsString()
+  @ApiProperty({ description: '자기소개' })
   introduction: string;
 
   @IsString()
+  @ApiProperty({ description: '지원동기' })
   motivation: string;
 
   @IsString()
+  @ApiProperty({ description: '기대활동' })
   expectedActivities: string;
 
   @IsString()
+  @ApiProperty({ description: '지원이유' })
   reasonToChoose: string;
 }
 
-export type CreateFormResponseDto = Omit<FormResponseDto, 'id'>;
-export type UpdateFormResponseDtoType = Partial<CreateFormResponseDto>;
+export type CreateFormResponseDtoType = Omit<FormResponseDto, 'id'>;
+export type UpdateFormResponseDtoType = Partial<CreateFormResponseDtoType>;
+
+export class CreateFormResponseDto extends OmitType(FormResponseDto, ['uuid'] as const) {
+}
+
+export class UpdateFormResponseDto extends PartialType(CreateFormResponseDto) {
+}
