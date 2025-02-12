995848d656b1cfe180e9f8124d3a4bc2543787da
Yuchan Han <serltretu24@gmail.com>
2025-02-12 15:37:07 +0900

    ✨ Add response handling endpoints to ApplyFormPublicController for improved form response management

--- packages/api/src/form/controllers/form.public.controller.ts
+++ packages/api/src/form/controllers/form.public.controller.ts
@@ -1,6 +1,18 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { Member } from '@tapie-kr/api-database';
+import { Response } from 'express';
 import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
+import { CreateFormResponseDto, UpdateFormResponseDto } from '@/form/dto/response.dto';
 import { ApplyFormService } from '@/form/form.service';
 
 @Controller('form')
@@ -9,9 +21,33 @@ import { ApplyFormService } from '@/form/form.service';
 export class ApplyFormPublicController {
   constructor(private readonly applyFormService: ApplyFormService) {
   }
-  @Get('active')
-  @ApiOperation({ summary: '활성화된 지원 폼 가져오기' })
-  getActiveForm() {
+  @Get()
+  async findActiveForm() {
     return this.applyFormService.getActiveForm();
   }
+  @Post(':id/response')
+  async createResponse(@Param('id') id: number, @Req() req: Response & {
+    user: Member;
+  }, @Body() createResponseDto: CreateFormResponseDto) {
+    return this.applyFormService.createResponse(id, req.user.uuid, createResponseDto);
+  }
+  @Get(':id/response')
+  async findResponse(@Param('id') id: number, @Req() req: Response & {
+    user: Member;
+  }) {
+    return this.applyFormService.findResponse(id, req.user.uuid);
+  }
+  @Patch(':id/response')
+  async updateResponse(@Param('id') id: number, @Req() req: Response & {
+    user: Member;
+  }, @Body() _updateResponseDto: UpdateFormResponseDto) {
+    return this.applyFormService.updateResponse(id, req.user.uuid);
+  }
+  @Post(':id/response/apply')
+  async applyForm(@Param('id') id: number, @Req() req: Response & {
+    user: Member;
+  }) {
+    return this.applyFormService.createResponse(id, req.user.uuid);
+  }
 }
+
