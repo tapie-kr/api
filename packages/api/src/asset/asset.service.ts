03c675e30d81e47875c5be0144ff589d2ca27af8
Yuchan Han <serltretu24@gmail.com>
2025-02-13 03:19:12 +0900

    ✨ Refactor AssetService to integrate caching and streamline Minio client usage

--- packages/api/src/asset/asset.service.ts
+++ packages/api/src/asset/asset.service.ts
@@ -1,13 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
+import { Inject, Injectable, UseInterceptors } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import Minio from 'minio';
+import { Client as MinioClient } from 'minio';
 import { AssetRepository } from '@/asset/asset.repository';
 import { FileType } from '@/asset/types/fileType';
 
 @Injectable()
+@UseInterceptors(CacheInterceptor)
 export class AssetService {
-  constructor(private readonly configService: ConfigService,
+  private readonly minioClient: MinioClient;
+  private readonly PRESIGNED_URL_EXPIRY = 2 * 60 * 60;
+
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private readonly configService: ConfigService,
     private readonly assetRepository: AssetRepository) {
+    this.minioClient = new MinioClient({
+      endPoint:  this.configService.get('MINIO_URL'),
+      useSSL:    true,
+      accessKey: this.configService.get('MINIO_ACCESS_KEY'),
+      secretKey: this.configService.get('MINIO_SECRET_KEY'),
+    });
+  }
+  @CacheTTL(1800)
+  async getFileWithUrl(uuid: string) {
+    const asset = await this.assetRepository.getAsset(uuid);
+    const presignedUrl = await this.minioClient.presignedUrl('GET', this.configService.get('MINIO_BUCKET_NAME'), asset.path, this.PRESIGNED_URL_EXPIRY);
+
+    return {
+      asset, presignedUrl,
+    };
   }
   async uploadFile(file: File, fileName: string, type: FileType, assetName?: string) {
     const url = await this.uploadFileToMinio(file, fileName, type);
@@ -15,19 +36,12 @@ export class AssetService {
     return this.assetRepository.createAsset(url, assetName);
   }
   async uploadFileToMinio(file: File, fileName: string, type: FileType) {
-    const minioClient = new Minio.Client({
-      endPoint:  this.configService.get('MINIO_URL'),
-      useSSL:    true,
-      accessKey: this.configService.get('MINIO_ACCESS_KEY'),
-      secretKey: this.configService.get('MINIO_SECRET_KEY'),
-    });
-
     const buffer = Buffer.from(await file.arrayBuffer());
     const path = type ? `${type}/${fileName}` : fileName;
     const BUCKET_NAME = this.configService.get('MINIO_BUCKET_NAME');
 
-    await minioClient.putObject(BUCKET_NAME, path, buffer, file.size);
+    await this.minioClient.putObject(BUCKET_NAME, path, buffer, file.size);
 
-    return `https://${this.configService.get('MINIO_URL')}/${BUCKET_NAME}/${path}`;
+    return path;
   }
 }
