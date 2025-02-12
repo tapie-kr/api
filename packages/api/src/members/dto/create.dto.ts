ab9861a0842126b053c560dcf274b27f35fbf655
Yuchan Han <serltretu24@gmail.com>
2025-02-12 15:35:53 +0900

    ✨ Add CreateMemberDto interface for member creation data transfer

---
+++ packages/api/src/members/dto/create.dto.ts
@@ -0,0 +1,9 @@
+import { MemberRole, MemberUnit } from '@tapie-kr/api-database';
+
+export interface CreateMemberDto {
+  googleEmail: string;
+  name:        string;
+  role:        MemberRole;
+  unit:        MemberUnit;
+  username?:   string;
+}
