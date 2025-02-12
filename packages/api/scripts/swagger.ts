b186cff31d3e2a1f02c8f57185b5e30342d2de64
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-13 05:03:52 +0900

    ✨ Update OpenAPI documentation and add BUMP_TOKEN to .env.example; enhance Swagger generation script

--- packages/api/scripts/swagger.ts
+++ packages/api/scripts/swagger.ts
@@ -3,21 +3,39 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { writeFileSync } from 'fs';
+import * as readline from 'node:readline';
 import { AppModule } from '../src/app.module';
 
 async function generateSwaggerSpec() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
   const app = await NestFactory.create(AppModule);
 
-  const config = (new DocumentBuilder)
+  const version = await new Promise<string>((resolve) => {
+    rl.question('Enter the API version: ', (answer) => {
+      resolve(answer);
+      rl.close();
+    });
+  });
+
+  const config = new DocumentBuilder()
     .setTitle('TAPIE API')
     .setDescription('TAPIE System API')
-    .setVersion('1.0')
-    .addBearerAuth({
-        type:         'http',
-        scheme:       'bearer',
+    .setVersion(version)
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
         bearerFormat: 'JWT',
       },
-      'accessToken')
+      'accessToken',
+    )
+    .addServer('http://localhost:8877', 'Local Development')
+    .addServer('https://tapie-api-development.vport.dev', 'Remote Development')
+    .addServer('https://api.tapie.kr', 'Remote Production')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
@@ -27,13 +45,12 @@ async function generateSwaggerSpec() {
   await app.close();
 }
 
-generateSwaggerSpec().then(() => {
-  console.log('Swagger spec generated');
-
-  process.exit(0);
-})
-  .catch(error => {
+generateSwaggerSpec()
+  .then(() => {
+    console.log('Swagger spec generated');
+    process.exit(0);
+  })
+  .catch((error) => {
     console.error('Error generating swagger spec', error);
-
     process.exit(1);
   });
