d43893e41afb984647f7996742624fac66992ca9
Yuchan Han <serltretu24@gmail.com>
2025-02-13 05:31:27 +0900

    ✨ Refactor members module structure: consolidate MembersModule and remove ProfileLink functionality

--- packages/api/src/auth/auth.module.ts
+++ packages/api/src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { MembersModule } from '@/members/module/members.module';
+import { MembersModule } from '@/members/members.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { GoogleStrategy } from './strategies/google.strategy';
