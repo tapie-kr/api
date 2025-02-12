f3605d7e5d8aadba2c8365a1ecd26f2876dc9cf7
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-13 05:24:00 +0900

    ✨ Add favicon and integrate swagger-themes for enhanced Swagger UI customization

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,11 +1,12 @@
-import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from '@/app.module';
 import { GlobalExceptionFilter } from '@/common/filters/global-exception.filter';
 import { PrismaExceptionFilter } from '@/common/filters/prisma-exception.filter';
 import { TransformInterceptor } from '@/common/interceptors/transform.interceptor';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,59 +14,73 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const isProduction = configService.get('NODE_ENV') === 'production';
 
-  app.useGlobalFilters(new GlobalExceptionFilter);
+  app.useGlobalFilters(new GlobalExceptionFilter());
 
-  app.useGlobalFilters(new PrismaExceptionFilter);
+  app.useGlobalFilters(new PrismaExceptionFilter());
 
-  app.useGlobalInterceptors(new TransformInterceptor);
+  app.useGlobalInterceptors(new TransformInterceptor());
 
-  app.useGlobalPipes(new ValidationPipe({
-    transform:            true,
-    whitelist:            true,
-    forbidNonWhitelisted: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
-  app.enableCors({ origin: isProduction
-    ? [
-      'https://tapie.kr/',
-      'https://api.tapie.kr/',
-      'https://admin.tapie.kr/',
-      'https://lms.tapie.kr/',
-    ]
-    : '*' });
+  app.enableCors({
+    origin: isProduction
+      ? [
+          'https://tapie.kr/',
+          'https://api.tapie.kr/',
+          'https://admin.tapie.kr/',
+          'https://lms.tapie.kr/',
+        ]
+      : '*',
+  });
 
   app.enableVersioning({
-    type:           VersioningType.URI,
+    type: VersioningType.URI,
     defaultVersion: '1',
   });
 
-  const config = (new DocumentBuilder)
+  const theme = new SwaggerTheme();
+
+  const config = new DocumentBuilder()
     .setTitle('TAPIE API')
     .setDescription('TAPIE System API')
-    .setVersion('1.0')
-    .addBearerAuth({
-      type:         'http',
-      scheme:       'bearer',
-      bearerFormat: 'JWT',
-    },
-    'accessToken')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'accessToken',
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api-docs', app, document, {
     jsonDocumentUrl: 'api-docs/json',
-    explorer:        true,
+    explorer: true,
     yamlDocumentUrl: 'api-docs/yaml',
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.ONE_DARK),
+    customfavIcon: 'https://s3.vport.dev/tapie-static/favicon.ico',
+    customSiteTitle: 'TAPIE API Swagger',
   });
 
   app.getHttpAdapter().get('/', (_req, res) => {
     res.json({
-      status: 'ok', availableVersions: ['v1'],
+      status: 'ok',
+      availableVersions: ['v1'],
     });
   });
 
-  await app.listen(isProduction ? Number(configService.get('PORT') || 3000) : 8877, '0.0.0.0');
+  await app.listen(
+    isProduction ? Number(configService.get('PORT') || 3000) : 8877,
+    '0.0.0.0',
+  );
 
   logger.log(`Server running on ${await app.getUrl()}`);
 }
