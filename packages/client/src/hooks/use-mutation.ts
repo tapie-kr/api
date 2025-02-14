4f640dc78fee7db31baa90d4e286ce7bb4228443
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-15 02:31:55 +0900

    ✨ Refactor import statements and enhance code formatting in useMutation and useQuery hooks

--- packages/client/src/hooks/use-mutation.ts
+++ packages/client/src/hooks/use-mutation.ts
@@ -1,16 +1,20 @@
-import { useMutation as useTanstackMutation, UseMutationOptions } from '@tanstack/react-query';
+import {
+  UseMutationOptions,
+  useMutation as useTanstackMutation,
+} from '@tanstack/react-query';
 import { AxiosRequestConfig } from 'axios';
-import { apiRequest } from '../request'
+import { apiRequest } from '../request';
 
 export function useMutation<T, V = unknown>(
   config: Omit<AxiosRequestConfig, 'data'>,
-  options?: UseMutationOptions<T, unknown, V>
+  options?: UseMutationOptions<T, unknown, V>,
 ) {
   return useTanstackMutation<T, unknown, V>({
-    mutationFn: (variables) => apiRequest<T>({
-      ...config,
-      data: variables,
-    }),
+    mutationFn: (variables) =>
+      apiRequest<T>({
+        ...config,
+        data: variables,
+      }),
     ...options,
   });
 }
--- packages/client/src/hooks/use-query.ts
+++ packages/client/src/hooks/use-query.ts
@@ -1,11 +1,14 @@
-import { useQuery as useTanstackQuery, UseQueryOptions } from '@tanstack/react-query';
+import {
+  UseQueryOptions,
+  useQuery as useTanstackQuery,
+} from '@tanstack/react-query';
 import { AxiosRequestConfig } from 'axios';
-import { apiRequest } from '../request'
+import { apiRequest } from '../request';
 
 export function useQuery<T>(
-  queryKey: any[],
+  queryKey: string[],
   config: AxiosRequestConfig,
-  options?: UseQueryOptions<T>
+  options?: UseQueryOptions<T>,
 ) {
   return useTanstackQuery<T>({
     queryKey,
