f48bcc6523e8d3e15f8311caade16fe7ec916608
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-15 03:26:13 +0900

    ✨ Enhance useMutation to accept HttpMethod and add useCreateApplyForm for form submissions

--- packages/client/src/hooks/use-mutation.ts
+++ packages/client/src/hooks/use-mutation.ts
@@ -3,10 +3,11 @@ import {
   useMutation as useTanstackMutation,
 } from '@tanstack/react-query';
 import { AxiosRequestConfig } from 'axios';
+import { HttpMethod } from '../constants/http-method';
 import { apiRequest } from '../request';
 
 export function useMutation<T, V = unknown>(
-  config: Omit<AxiosRequestConfig, 'data'>,
+  config: Omit<AxiosRequestConfig, 'data'> & { method: HttpMethod },
   options?: UseMutationOptions<T, unknown, V>,
 ) {
   return useTanstackMutation<T, unknown, V>({
---
+++ packages/client/src/request/form-private.ts
@@ -0,0 +1,24 @@
+import { useQueryClient } from '@tanstack/react-query';
+import { HttpMethod } from '../constants/http-method';
+import { FormPrivateQueryKeys } from '../constants/query-keys';
+import { useMutation } from '../hooks/use-mutation';
+import { CreateApplyForm } from '../schemas/form';
+
+export const useCreateApplyForm = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation<CreateApplyForm, CreateApplyForm>(
+    {
+      method: HttpMethod.POST,
+      url: '/form/admin',
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: FormPrivateQueryKeys.FORM });
+      },
+      onError: (error) => {
+        console.error('Error creating applyForm:', error);
+      },
+    },
+  );
+};
