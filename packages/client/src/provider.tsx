2a04b0d456d75a124353ae4f90c571f19ced3dda
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-15 01:27:44 +0900

    ✨ Add React Query Devtools support in TapieApiProvider and update dependencies

--- packages/client/src/provider.tsx
+++ packages/client/src/provider.tsx
@@ -1,16 +1,22 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 
 interface TapieApiProviderProps {
   children: React.ReactNode;
+  devtools?: boolean;
+  initialIsOpen?: boolean;
 }
 
 const queryClient = new QueryClient();
 
 export default function TapieApiProvider(props: TapieApiProviderProps) {
+  const { devtools = false, initialIsOpen = false } = props;
+
   return (
     <QueryClientProvider client={queryClient}>
       {props.children}
+      {devtools && <ReactQueryDevtools initialIsOpen={initialIsOpen} />}
     </QueryClientProvider>
   );
 }
