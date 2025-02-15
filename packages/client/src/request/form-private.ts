9ecc08b4063c176b770741339d9282443f644ea7
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-15 19:22:05 +0900

    ✨ Add form query hooks for listing, retrieving, and deleting forms; update mutation for form updates

--- packages/client/src/request/form-private.ts
+++ packages/client/src/request/form-private.ts
@@ -3,7 +3,30 @@ import { HttpMethod } from '../constants/http-method';
 import { FormPrivateQueryKeys } from '../constants/query-keys';
 import { FormId } from '../constants/query-keys/form-private';
 import { useMutation } from '../hooks/use-mutation';
-import { CreateForm, CreateFormResponse } from '../schemas/form';
+import { useQuery } from '../hooks/use-query';
+import {
+  CreateForm,
+  CreateFormResponse,
+  DeleteFormResponse,
+  FormListResponse,
+  FormResponse,
+  UpdateForm,
+  UpdateFormResponse,
+} from '../schemas/form';
+
+export const useFormList = () => {
+  return useQuery<FormListResponse>(FormPrivateQueryKeys.FORM, {
+    method: HttpMethod.GET,
+    url: '/form/admin',
+  });
+};
+
+export const useForm = (id: FormId) => {
+  return useQuery<FormResponse>(FormPrivateQueryKeys.FORM, {
+    method: HttpMethod.GET,
+    url: `/form/admin/${id}`,
+  });
+};
 
 export const useCreateForm = () => {
   const queryClient = useQueryClient();
@@ -27,7 +50,7 @@ export const useCreateForm = () => {
 export const useUpdateForm = (id: FormId) => {
   const queryClient = useQueryClient();
 
-  return useMutation<CreateForm, CreateForm>(
+  return useMutation<UpdateFormResponse, UpdateForm>(
     {
       method: HttpMethod.PATCH,
       url: `/form/admin/${id}`,
@@ -40,6 +63,27 @@ export const useUpdateForm = (id: FormId) => {
       },
       onError: (error) => {
         console.error('Error updating form:', error);
+      },
+    },
+  );
+};
+
+export const useDeleteForm = (id: FormId) => {
+  const queryClient = useQueryClient();
+
+  return useMutation<DeleteFormResponse, unknown>(
+    {
+      method: HttpMethod.DELETE,
+      url: `/form/admin/${id}`,
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: FormPrivateQueryKeys.FORM_DETAIL(id),
+        });
+      },
+      onError: (error) => {
+        console.error('Error deleting form:', error);
       },
     },
   );
