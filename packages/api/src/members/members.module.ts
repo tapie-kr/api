836f96f3c13c6cfc898d4df91429a38c0d421499
Yuchan Han <serltretu24@gmail.com>
2025-02-17 05:07:00 +0900

    ✨ Add AssetModule and MemberPrivateController to MembersModule

--- packages/api/src/members/members.module.ts
+++ packages/api/src/members/members.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AssetModule } from '@/asset/asset.module';
 import { PrismaService } from '@/common/prisma/prisma.service';
+import { MemberPrivateController } from '@/members/controllers/member.private.controller';
 import { MemberPublicController } from '@/members/controllers/member.public.controller';
 import { MemberRepository } from '@/members/repository/member.repository';
 import { MembersService } from '@/members/service/members.service';
 
 @Module({
-  imports:     [],
-  controllers: [MemberPublicController],
+  imports:     [AssetModule],
+  controllers: [MemberPublicController, MemberPrivateController],
   providers:   [
     MemberRepository,
     MembersService,
