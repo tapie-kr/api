9f9333e62be7a6335277fa622826b0af09fdf2a0
Yuchan Han <serltretu24@gmail.com>
2025-02-17 17:43:45 +0900

    ✨ Refactor competition DTOs to improve structure and add preview and create types

--- packages/api/src/awards/dto/competition.dto.ts
+++ packages/api/src/awards/dto/competition.dto.ts
@@ -1,18 +1,31 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
 import { IsString, IsUUID } from 'class-validator';
-import { MemberAwardDto } from '@/members/dto/member-award.dto';
+import { AwardDto } from '@/awards/dto/award.dto';
 
-export class MemberCompetitionDto {
+export class CompetitionDto {
   @IsUUID()
-  @ApiProperty({ description: '대회 UUID' })
+  @ApiProperty({
+    description: '대회 UUID', example: '123e4567-e89b-12d3-a456-426614174000',
+  })
   uuid: string;
 
   @IsString()
-  @ApiProperty({ description: '대회 이름' })
+  @ApiProperty({
+    description: '대회 이름', example: 'STA+C 1981',
+  })
   name: string;
 
   @ApiProperty({
-    type: () => MemberAwardDto, isArray: true,
+    type: () => AwardDto, isArray: true,
   })
-  awards?: MemberAwardDto[];
+  awards?: AwardDto[];
+}
+
+export class CompetitionPreviewDto extends OmitType(CompetitionDto, ['awards'] as const) {
+}
+
+export class CreateCompetitionDto extends OmitType(CompetitionDto, ['uuid', 'awards'] as const) {
+}
+
+export class ConnectCompetitionDto extends PartialType(CompetitionPreviewDto) {
 }
