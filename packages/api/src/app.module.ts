302ef2aa66e57632b4d7d7a4dc22d7b0e4457864
Yuchan Han <serltretu24@gmail.com>
2025-02-17 17:42:45 +0900

    ✨ Add AwardsModule to the main application module for managing awards and competitions

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AssetModule } from '@/asset/asset.module';
 import { AuthModule } from '@/auth/auth.module';
+import { AwardsModule } from '@/awards/awards.module';
 import { FormModule } from '@/form/form.module';
 import { MembersModule } from '@/members/members.module';
 
@@ -11,17 +12,22 @@ import { MembersModule } from '@/members/members.module';
     isGlobal:    true,
     envFilePath: ['.env', '.env.development'],
   }),
-  MembersModule,
-  CacheModule.register({ isGlobal: true }),
-  AuthModule,
 
   // Members
+  MembersModule,
+
+  // Core
+  CacheModule.register({ isGlobal: true }),
+  AuthModule,
 
   // Form
   FormModule,
 
   // Minio (S3)
   AssetModule,
+
+  // Awards, Competitions
+  AwardsModule,
 ] })
 export class AppModule {
 }
