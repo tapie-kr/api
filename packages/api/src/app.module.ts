6bcb9901d21f6e510fb876ea87b1b6ce91319baf
Yuchan Han <serltretu24@gmail.com>
2025-02-21 11:48:50 +0900

    ✨ Replace AwardsModule with PortfolioModule in app.module.ts to update module dependencies

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -3,9 +3,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AssetModule } from '@/asset/asset.module';
 import { AuthModule } from '@/auth/auth.module';
-import { AwardsModule } from '@/awards/awards.module';
 import { FormModule } from '@/form/form.module';
 import { MembersModule } from '@/members/members.module';
+import { PortfolioModule } from '@/portfolio/portfolio.module';
 
 @Module({ imports: [
   ConfigModule.forRoot({
@@ -27,7 +27,7 @@ import { MembersModule } from '@/members/members.module';
   AssetModule,
 
   // Awards, Competitions
-  AwardsModule,
+  PortfolioModule,
 ] })
 export class AppModule {
 }
