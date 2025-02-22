04063fa2f6367a61e37d2e9f07e810c092904884
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-02-23 03:24:30 +0900

    ✨ Add member and member-private query keys, schemas, and hooks for enhanced member management

--- packages/client/src/schemas/member.ts
+++ packages/client/src/schemas/member.ts
@@ -2,8 +2,9 @@ import { MemberRole } from "@/constants/member-role"
 import { UnitType } from "@/constants/unit-type"
 import { z } from "zod"
 import { BaseResponse } from "@/schemas/base"
+import { SkillType } from "@/constants/skill-type"
 
-export const publicMemberSchema = z.object({
+export const memberSchema = z.object({
     uuid: z.string(),
     name: z.string(),
     username: z.string(),
@@ -14,5 +15,64 @@ export const publicMemberSchema = z.object({
     profileUrl: z.string(),
 });
 
-export const publicMemberListResponseSchema = z.array(publicMemberSchema);
-export type PublicMemberListResponse = BaseResponse<typeof publicMemberListResponseSchema>;
+export const createMemberScheme = z.object({
+    name: z.string(),
+    username: z.string(),
+    googleEmail: z.string(),
+    role: z.nativeEnum(MemberRole),
+    unit: z.nativeEnum(UnitType),
+    generation: z.number(),
+})
+
+export const memberLinkSchema = z.object({
+    icon: z.string(),
+    label: z.string(),
+    href: z.string(),
+})
+
+export const memberSkillSchema = z.object({
+    isVerified: z.boolean(),
+    isLearning: z.boolean(),
+    skill: z.object({
+        uuid: z.string(),
+        icon: z.string(),
+        name: z.string(),
+        type: z.nativeEnum(SkillType),
+    })
+})
+
+export const memberSkillResponseSchema = z.object({
+    isVerified: z.boolean(),
+    isLearning: z.boolean(),
+    skill: z.object({
+        uuid: z.string(),
+        icon: z.string(),
+        name: z.string(),
+        type: z.nativeEnum(SkillType),
+        createdAt: z.date(),
+        updatedAt: z.date(),
+    })
+})
+
+export const memberListResponseSchema = z.array(memberSchema);
+export type PublicMemberListResponse = BaseResponse<typeof memberListResponseSchema>;
+
+export type MemberResponse = BaseResponse<typeof memberSchema>;
+export type MemberListResponse = BaseResponse<typeof memberListResponseSchema>;
+
+export type CreateMember = z.infer<typeof createMemberScheme>;
+
+export const updateMemberSchema = memberSchema.partial();
+export type UpdateMember = z.infer<typeof updateMemberSchema>;
+
+export type MemberLink = z.infer<typeof memberLinkSchema>;
+export type MemberLinkResponse = BaseResponse<typeof memberLinkSchema & {uuid: string, profileUrl: string}>;
+
+export const updateMemberLinkSchema = memberLinkSchema.partial();
+export type UpdateMemberLink = z.infer<typeof updateMemberLinkSchema>;
+
+export type MemberSkillRequest = z.infer<typeof memberSkillSchema>;
+export type MemberSkillResponse = BaseResponse<typeof memberSkillResponseSchema>;
+
+export const updateMemberSkillSchema = memberSkillSchema.partial();
+export type UpdateMemberSkill = z.infer<typeof updateMemberSkillSchema>;
