e73a942716142ee5fb77d60751df3dee42ac803a
Yuchan Han <serltretu24@gmail.com>
2025-02-24 01:31:16 +0900

    ✨ Enhance ProjectRepository: Add createProject method with error handling for portfolio creation

--- packages/api/src/projects/repository/project.repository.ts
+++ packages/api/src/projects/repository/project.repository.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Prisma } from '@tapie-kr/api-database';
+import { PrismaOperationFailedError, toTypedPrismaError } from '@/common/prisma/prisma.exception';
 import { PrismaService } from '@/common/prisma/prisma.service';
+import { CreatePortfolioPrismaDto } from '@/projects/dto/portfolio.dto';
 
 @Injectable()
 export class ProjectRepository {
@@ -7,5 +10,23 @@ export class ProjectRepository {
   }
   async getAllProjects() {
     return this.prisma.portfolio.findMany();
+  }
+  async createProject(data: CreatePortfolioPrismaDto & {
+    membersUUID: string[];
+  }) {
+    try {
+      const createData: Prisma.XOR<Prisma.PortfolioCreateInput, Prisma.PortfolioUncheckedCreateInput> = {
+        ...data,
+        members: { connect: data.membersUUID.map(uuid => ({ uuid })) },
+      };
+    } catch (error) {
+      const prismaException = toTypedPrismaError(error);
+
+      if (prismaException instanceof PrismaOperationFailedError) {
+        throw new BadRequestException('입력된 데이터 값이 잘못되었습니다.');
+      }
+
+      throw error;
+    }
   }
 }
