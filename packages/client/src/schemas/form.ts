ee3a4c5a4502f31e71ffa9de46616af768e8f24b
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-24 12:51:05 +0900

    ✨ Bump version to 0.0.4 and refactor form schemas for improved readability

--- packages/client/src/schemas/form.ts
+++ packages/client/src/schemas/form.ts
@@ -1,47 +1,46 @@
-import { z } from 'zod';
 import { koreanPhoneNumberPattern } from '@/constants/regex';
 import { UnitType } from '@/constants/unit-type';
 import { BaseResponse } from '@/schemas/base';
+import { z } from 'zod';
 
 export const formDetailScheme = z.object({
-  uuid:               z.string().uuid(),
-  formId:             z.number(),
-  memberUUID:         z.string().uuid(),
+  uuid: z.string().uuid(),
+  formId: z.number(),
+  memberUUID: z.string().uuid(),
   portfolioAssetUUID: z.string().uuid(),
-  name:               z.string().min(1, '이름은 필수입니다.'),
-  studentId:          z.string(),
-  googleEmail:        z
+  name: z.string().min(1, '이름은 필수입니다.'),
+  studentId: z.string(),
+  googleEmail: z
     .string()
     .email('올바른 이메일 형식이 아닙니다.')
     .endsWith('@gmail.com', '구글 이메일(@gmail.com)만 사용 가능합니다.'),
-  unit:        z.nativeEnum(UnitType),
+  unit: z.nativeEnum(UnitType),
   phoneNumber: z
     .string()
     .regex(koreanPhoneNumberPattern)
     .min(1, '전화번호는 필수입니다.'),
-  introduction:       z.string().min(1, '자기소개는 필수입니다.'),
-  motivation:         z.string().min(1, '지원동기는 필수입니다.'),
+  introduction: z.string().min(1, '자기소개는 필수입니다.'),
+  motivation: z.string().min(1, '지원동기는 필수입니다.'),
   expectedActivities: z.string().min(1, '기대하는 활동은 필수입니다.'),
-  reasonToChoose:     z.string().min(1, '선택이유는 필수입니다.'),
-  createdAt:          z.date(),
-  updatedAt:          z.date(),
-  submitted:          z.boolean(),
+  reasonToChoose: z.string().min(1, '선택이유는 필수입니다.'),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  submitted: z.boolean(),
 });
 
 export const formResponseSchema = z.object({
-  id:       z.number(),
-  name:     z.string(),
+  id: z.number(),
+  name: z.string(),
   startsAt: z.date(),
-  endsAt:   z.date(),
-  active:   z.boolean(),
+  endsAt: z.date(),
+  active: z.boolean(),
 });
 
 export const formListResponseSchema = z.array(formResponseSchema);
 export const formDetailListResponseSchema = z.array(formDetailScheme);
 
 export const createFormSchema = formResponseSchema.omit({
-  id:     true,
-  active: true,
+  id: true,
 });
 
 export const updateFormSchema = formResponseSchema.omit({ id: true }).partial();
@@ -59,12 +58,12 @@ export type FormDetailListResponse = BaseResponse<
 
 // Public Form API
 export const createFormApplicationSchema = formDetailScheme.pick({
-  unit:               true,
-  phoneNumber:        true,
-  introduction:       true,
-  motivation:         true,
+  unit: true,
+  phoneNumber: true,
+  introduction: true,
+  motivation: true,
   expectedActivities: true,
-  reasonToChoose:     true,
+  reasonToChoose: true,
 });
 
 export const updateFormApplicationScheme =
