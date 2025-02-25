d9c9eeff1d7e672e13a903ca539cee1fa0901595
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-26 04:10:11 +0900

    ✨ Bump version to 0.0.17 and add authentication URL handling in ApiClient

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -2,10 +2,12 @@ import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 export class ApiClient {
   private instance: AxiosInstance;
+  private authURL: string;
 
   constructor() {
     const version = process.env.API_VERSION || 'v1';
     const hostname = process.env.API_HOSTNAME || 'http://localhost:8877/';
+    this.authURL = process.env.AUTH_URL || 'http://localhost:9876/';
     const baseURL = new URL(hostname);
 
     baseURL.pathname = version;
@@ -17,6 +19,16 @@ export class ApiClient {
   }
   public async request<T>(config: AxiosRequestConfig): Promise<T> {
     const response = await this.instance.request<T>(config);
+
+    if (response.status == 401) {
+      const refreshResponse = await this.instance.post('/auth/refresh');
+
+      if (refreshResponse.status == 404) {
+        location.href = this.authURL;
+      } else {
+        await this.request(config);
+      }
+    }
 
     return response.data;
   }
