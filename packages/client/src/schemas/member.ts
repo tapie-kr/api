a7b86fdf71dbb6e8b4181959f828f6a22318affe
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-26 06:02:28 +0900

    ✨ Bump version to 0.0.23 and refactor member schemas for improved structure and clarity

--- packages/client/src/schemas/member.ts
+++ packages/client/src/schemas/member.ts
@@ -1,18 +1,7 @@
-import { z } from "zod";
-import { MemberRole } from "@/constants/enum/member-role";
-import { MemberUnit } from "@/constants/enum/unit-type";
-import { BaseResponse } from "@/schemas/base";
-
-export const memberSchema = z.object({
-  uuid: z.string(),
-  name: z.string(),
-  username: z.string(),
-  role: z.nativeEnum(MemberRole),
-  unit: z.nativeEnum(MemberUnit),
-  generation: z.number(),
-  googleEmail: z.string(),
-  profileUri: z.string(),
-});
+import { MemberRole } from '@/constants/enum/member-role';
+import { MemberUnit } from '@/constants/enum/unit-type';
+import { BaseResponse } from '@/schemas/base';
+import { z } from 'zod';
 
 export const createMemberScheme = z.object({
   name: z.string(),
@@ -29,6 +18,22 @@ export const memberLinkSchema = z.object({
   href: z.string(),
 });
 
+export const memberLinkResponseSchema = z.object({
+  id: z.number(),
+  icon: z.string(),
+  label: z.string(),
+  href: z.string(),
+});
+
+export const memberAwardsResponseSchema = z.object({
+  uuid: z.string(),
+  competitionUUID: z.string(),
+  title: z.string(),
+  grade: z.number(),
+  gradeLabel: z.string(),
+  rewardedAt: z.string(),
+});
+
 export const memberSkillSchema = z.object({
   isVerified: z.boolean(),
   isLearning: z.boolean(),
@@ -41,6 +46,7 @@ export const memberSkillSchema = z.object({
 });
 
 export const memberSkillResponseSchema = z.object({
+  uuid: z.string(),
   isVerified: z.boolean(),
   isLearning: z.boolean(),
   skill: z.object({
@@ -53,16 +59,44 @@ export const memberSkillResponseSchema = z.object({
   }),
 });
 
-export const memberListResponseSchema = z.array(memberSchema);
+export const memberHistoryResponseSchema = z.object({
+  id: z.number(),
+  label: z.string(),
+  link: z.string(),
+  isImportant: z.boolean(),
+  releasedAt: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+});
+
+export const memberResponseSchema = z.object({
+  uuid: z.string(),
+  name: z.string(),
+  studentID: z.number(),
+  username: z.string(),
+  googleEmail: z.string(),
+  role: z.nativeEnum(MemberRole),
+  unit: z.nativeEnum(MemberUnit),
+  generation: z.number(),
+  profileUri: z.string(),
+  isGraduated: z.boolean(),
+  permissions: z.number(),
+  links: z.array(memberLinkResponseSchema),
+  awards: z.array(memberAwardsResponseSchema),
+  skills: z.array(memberSkillResponseSchema),
+  history: z.array(memberHistoryResponseSchema),
+});
+
+export const memberListResponseSchema = z.array(memberResponseSchema);
 
 export type PublicMemberListResponse = BaseResponse<
   typeof memberListResponseSchema
 >;
 
-export type MemberResponse = BaseResponse<typeof memberSchema>;
+export type MemberResponse = BaseResponse<typeof memberResponseSchema>;
 export type MemberListResponse = BaseResponse<typeof memberListResponseSchema>;
 export type CreateMember = z.infer<typeof createMemberScheme>;
-export const updateMemberSchema = memberSchema.partial();
+export const updateMemberSchema = memberResponseSchema.partial();
 export type UpdateMember = z.infer<typeof updateMemberSchema>;
 export type MemberLink = z.infer<typeof memberLinkSchema>;
 
@@ -84,6 +118,8 @@ export type MemberSkillResponse = BaseResponse<
 export const updateMemberSkillSchema = memberSkillSchema.partial();
 export type UpdateMemberSkill = z.infer<typeof updateMemberSkillSchema>;
 
-export type MemberType = z.infer<typeof memberSchema>;
-export type MemberLinkType = z.infer<typeof memberLinkSchema>;
-export type MemberSkillType = z.infer<typeof memberSkillSchema>;
+export type MemberType = z.infer<typeof memberResponseSchema>;
+export type MemberLinkType = z.infer<typeof memberLinkResponseSchema>;
+export type MemberAwardsType = z.infer<typeof memberAwardsResponseSchema>;
+export type MemberSkillType = z.infer<typeof memberSkillResponseSchema>;
+export type MemberHistoryType = z.infer<typeof memberHistoryResponseSchema>;
