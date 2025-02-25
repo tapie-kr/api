eb641f432cf04d3ff5d2a583f76c3a2027767867
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-26 05:24:10 +0900

    ✨ Bump version to 0.0.21 and enhance token refresh handling in ApiClient

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -16,23 +16,35 @@ export class ApiClient {
       baseURL: baseURL.toString(),
       timeout: Number(process.env.API_TIMEOUT) || 5000,
     });
+
+    this.instance.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (error.response && error.response.status === 401) {
+          console.log('Refreshing Token');
+
+          try {
+            const refreshResponse = await this.instance.post('/auth/refresh');
+
+            if (refreshResponse.status === 401) {
+              console.warn('Refresh token expired. Redirecting to auth page.');
+              window.location.href = this.authURL;
+            } else {
+              console.log('Token Refreshed. Retrying request...');
+              return this.instance.request(error.config);
+            }
+          } catch (refreshError) {
+            console.error('Refresh token request failed:', refreshError);
+            window.location.href = this.authURL;
+          }
+        }
+        return Promise.reject(error);
+      },
+    );
   }
+
   public async request<T>(config: AxiosRequestConfig): Promise<T> {
     const response = await this.instance.request<T>(config);
-
-    console.log('Response:', response);
-
-    if (response.status == 401) {
-      console.log('Refreshing Token');
-      const refreshResponse = await this.instance.post('/auth/refresh');
-
-      if (refreshResponse.status == 401) {
-        location.href = this.authURL;
-      } else {
-        console.log('Token Refreshed');
-        await this.request(config);
-      }
-    }
 
     return response.data;
   }
