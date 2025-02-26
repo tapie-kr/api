2ba49a5f0ac4d9baec13e890b252effdec92bd7c
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-02-26 20:10:44 +0900

    ✨ Bump version to 0.0.29 and enhance useFetch hook with caching and refresh functionality

--- packages/client/src/types/hooks/fetch.ts
+++ packages/client/src/types/hooks/fetch.ts
@@ -1,5 +1,6 @@
 export interface UseFetchResult<TData> {
   fetch:     () => Promise<void>;
+  refresh:   () => Promise<void>;
   data:      TData | null;
   error:     Error | null;
   isPending: boolean;
