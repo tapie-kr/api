eaac178af62b83f3596d71b1487bdf2d6beb7db9
iamfiro <appturbo102@gmail.com>
2025-02-27 22:29:13 +0900

    ✨ Rename YearFilterQuery to YearFilterParam for improved clarity in award list fetching

--- packages/client/src/request/competition/public.ts
+++ packages/client/src/request/competition/public.ts
@@ -1,14 +1,14 @@
 import useDynamicFetch from "@/hooks/use-dynamic-fetch";
 import { PublicAwardListResponse } from "@/schemas/competition";
 
-type YearFilterQuery = { year?: string };
+type YearFilterParam = { year?: string };
 
 /**
  * 모든 대회 수상 가져오기 (메인 페이지용)
  * @returns {PublicAwardListResponse} PublicAwardListResponse
  */
 export const useAwardList = () => {
-  return useDynamicFetch<PublicAwardListResponse, YearFilterQuery>(
+  return useDynamicFetch<PublicAwardListResponse, YearFilterParam>(
     ({ year }) => `/portfolio/awards?year=${year}`
   );
 };
