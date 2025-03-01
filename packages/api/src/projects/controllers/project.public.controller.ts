0902ae429e4aab55ea39a169db524e22a7c5e4bf
Yuchan Han <serltretu24@gmail.com>
2025-03-01 23:29:50 +0900

    ✨ Remove unused guards from ProjectPublicController to simplify access control.

--- packages/api/src/projects/controllers/project.public.controller.ts
+++ packages/api/src/projects/controllers/project.public.controller.ts
@@ -1,11 +1,8 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AssetService } from '@/asset/asset.service';
-import { PermissionGuard } from '@/auth/guards/permission.guard';
-import { UserAuthGuard } from '@/auth/guards/user-auth.guard';
 import { ProjectService } from '@/projects/projects.service';
 
 @Controller('projects')
-@UseGuards(UserAuthGuard, PermissionGuard)
 export class ProjectPublicController {
   constructor(private readonly projectService: ProjectService,
     private readonly assetService: AssetService) {
