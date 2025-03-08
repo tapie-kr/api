b1543b1622e721b79362ca9350515e63c35404ea
Yuchan Han <serltretu24@gmail.com>
2025-03-09 02:59:48 +0900

    feat(instrument): simplify Sentry initialization in non-production environments

--- packages/api/src/instrument.ts
+++ packages/api/src/instrument.ts
@@ -1,9 +1,5 @@
 import Sentry from '@sentry/nestjs';
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 if (process.env.NODE_ENV !== 'production') {
-  Sentry.init({
-    dsn:          process.env.SENTRY_DSN,
-    integrations: [nodeProfilingIntegration()],
-  });
+  Sentry.init({ dsn: process.env.SENTRY_DSN });
 }
