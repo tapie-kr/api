d88923c9500e7b4e4d0abec1ebb0c7df2ab2b274
Yuchan Han <serltretu24@gmail.com>
2025-03-08 12:25:21 +0900

    OPS-9 fix(main): update import path for instrument module
    Signed-off-by: Yuchan Han <serltretu24@gmail.com>

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -10,7 +10,7 @@ import { GlobalExceptionFilter } from '@/common/filters/global-exception.filter'
 import { PrismaExceptionFilter } from '@/common/filters/prisma-exception.filter';
 import { TransformInterceptor } from '@/common/interceptors/transform.interceptor';
 
-import '@/instrument';
+import './instrument';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
