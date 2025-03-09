ad91e74999b16ca0f5b5a316f0c7bcb17795a780
iamfiro <appturbo102@gmail.com>
2025-03-09 20:22:36 +0900

    ✨ Enable withCredentials for Axios instance in ApiClient

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -18,6 +18,7 @@ export class ApiClient {
     this.instance = axios.create({
       baseURL: baseURL.toString(),
       timeout: Number(process.env.API_TIMEOUT) || 5000,
+      withCredentials: true,
     });
 
     this.instance.interceptors.response.use(
